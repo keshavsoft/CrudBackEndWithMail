42dbda8e2a78427aadea9ee9586819729daf2be8
Srivasu215 <sri.vasu215@gmail.com>
2025-04-24 12:33:17 +0530

    JsonFile ReadFile

--- JsonFile/routes.js
+++ JsonFile/routes.js
@@ -3,7 +3,9 @@ import express from 'express';
 var router = express.Router();
 
 import { router as routerFromCreateFile } from './CreateFile/routes.js';
+import { router as routerFromReadFile } from './ReadFile/routes.js';
 
 router.use('/CreateFile', routerFromCreateFile);
+router.use('/ReadFile', routerFromReadFile);
 
 export { router };
